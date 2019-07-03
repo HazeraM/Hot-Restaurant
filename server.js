be135d8522e31030820439bc9531ad460b945b8d
Roshini Jaiswal <roshinijaiswal@dyn-160-39-11-194.dyn.columbia.edu>
2019-07-03 14:11:49 -0400

    set up a server and added a git ignore file

---
+++ server.js
@@ -0,0 +1,13 @@
+//creating a basic server
+var express = require('express')
+var app = express()
+var PORT = process.env.PORT || 3000
+
+//app.get('/', (req, res) => res.send('Hello World!'))
+
+//SETTING UP THE EXPRESS APP To HANDLE DATA PARSING
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.listen(PORT, function() {
+    console.log("Listening On PORT" + PORT);
+});
