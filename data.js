2b5395a3363298cd3d865e3b818b3befa23b7682
Christian Suleiman <cisuleiman@gmail.com>
2019-07-08 12:03:53 -0400

    added some functionality for making reservation

---
+++ data.js
@@ -0,0 +1,17 @@
+var reservations = [{
+    name: "chris", 
+    email: "hi@gmail.com",
+    phone: "123-4568",
+    id: "cristo123"
+},
+
+{
+    name: 'roshini',
+    email: "ro@gmail.com",
+    phone: "222-2222",
+    id: "rosh"
+}];
+
+module.exports = reservations;
+
+
--- server.js
+++ server.js
@@ -4,14 +4,21 @@ var path = require('path');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var reservations = require("./data.js");
+console.log(reservations);
+
 //app.get('/', (req, res) => res.send('Hello World!'))
 
 //SETTING UP THE EXPRESS APP To HANDLE DATA PARSING
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
 app.listen(PORT, function() {
-    console.log("Listening On PORT" + PORT);
+    console.log("Listening On PORT:" + PORT);
 });
+
+
 app.get("/homepage", function(req, res) {
     res.sendFile(path.join(__dirname, "homepage.html"));
 });
@@ -23,3 +30,20 @@ app.get("/reservation/form", function(req, res) {
 app.get("/reservation/view", function(req, res) {
     res.sendFile(path.join(__dirname, "reservationView.html"));
 });
+
+
+$("#submit-reservation").on("click", function() {
+    var userName = $("#name-input").val().trim();
+    var userEmail =  $("email-input").val().trim();
+    var userPhone = $("phone-input").val().trim();
+    var userId = $("id-input").val().trim();
+
+    var newReservation = {};
+    newReservation.name = userName;
+    newReservation.email = userEmail;
+    newReservation.phone = userPhone;
+    newReservation.id = userId;
+
+    reservations.push(newReservation);
+    console.log(reservations);    
+})
